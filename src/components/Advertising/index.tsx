648ab25bf1137d38872c6b5d5b9238c47076f03c
caio-couto <cavalcantecaio.couto@gmail.com>
2025-03-17 15:09:16 -0300

    feat: improvement ad

--- src/components/Advertising/index.tsx
+++ src/components/Advertising/index.tsx
@@ -1,13 +1,10 @@
 export default function Advertising() {
   return (
     <div className="py-8 px-2">
-      <div className="uppercase text-center text-xs mb-2">Advertising</div>
-      <div className="">
       <div id="av_top_wrapper" style={{ width: "100%", marginTop: "2rem", marginBottom: "2rem", minHeight: "400px" }}>
         <div>
-            <p style={{ fontSize: "10px", textTransform: "uppercase", textAlign: "center" }}> Anúncios </p>
-              <div id="av_top"></div>
-            </div>
+          <p style={{ fontSize: "10px", textTransform: "uppercase", textAlign: "center" }}> Anúncios </p>
+          <div id="av_top"></div>
         </div>
       </div>
     </div>
