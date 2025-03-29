784b593da76b9e862018365879ba19638143bd0a
caio-couto <cavalcantecaio.couto@gmail.com>
2025-03-29 02:11:11 -0300

    feat: add tracking code in shorts pages

--- src/app/shorts/layout.tsx
+++ src/app/shorts/layout.tsx
@@ -1,10 +1,16 @@
+import TrackingCode from "@/components/TrackingCode";
 
 export default function ShortsLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <body className="overflow-hidden md:overflow-auto md:w-screen md:h-screen">
-      <div className="shorts bg-transparent fixed md:relative bg-red-600 top-0 right-0 left-0 w-full h-full">
-        {children}
-      </div>
-    </body>
+    <>
+      <head>
+        <TrackingCode/>
+      </head>
+      <body className="overflow-hidden md:overflow-auto md:w-screen md:h-screen">
+        <div className="shorts bg-transparent fixed md:relative bg-red-600 top-0 right-0 left-0 w-full h-full">
+          {children}
+        </div>
+      </body>
+    </>
   );
 }
