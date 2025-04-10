ceb5610daaa249809fd37c7b708c97ac5ad7f83b
caio-couto <cavalcantecaio.couto@gmail.com>
2025-04-10 10:20:10 -0300

    fix: remove ad in next episode page

--- src/components/NextEpisode/index.tsx
+++ src/components/NextEpisode/index.tsx
@@ -13,8 +13,7 @@ interface NextEpisodeProps {
 export default function NextEpisode({ slideScreenRef, active, novel, handleClick  }: NextEpisodeProps) {
   return (
     <div className="w-full h-full pt-14 bg-black relative">
-      <div ref={slideScreenRef} className="h-full">
-        <Advertising/>
+      <div ref={slideScreenRef} className="relative top-1/2">
         <div className="px-4">
           <h2 className="font-semibold text-lg mb-2">Você está assistindo:</h2>
           <p>
@@ -22,11 +21,11 @@ export default function NextEpisode({ slideScreenRef, active, novel, handleClick
             vingança após ser abandonada, casando-se impulsivamente com o chefe de seu ex.
           </p>
         </div>
-      </div>
-      <div className="w-full px-2 py-3 absolute bottom-0 bg-black">
+      <div className="w-full px-2 py-3 bg-black mt-12">
         <button data-av-rewarded="true" onClick={handleClick} className="w-full mb-4 py-4 px-4 rounded-3xl bg-primary font-semibold cursor-pointer">
           Próximo episódio
         </button>
+      </div>
       </div>
     </div>
   );
